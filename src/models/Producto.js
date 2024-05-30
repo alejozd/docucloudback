0e9cf1f04601bbeacbddbfe53719b5b4310b66d9
Alejandro Zambrano <alejozd79@gmail.com>
2024-05-30 15:33:06 -0500

    productos

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const Producto = sequelize.define(
-  "Producto",
+  "producto",
   {
     idproducto: {
       type: DataTypes.INTEGER,
