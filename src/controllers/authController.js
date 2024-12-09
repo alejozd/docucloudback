3103940fb59b40f526c1483cfc66b1653847eb66
alejozd <alejozd79@gmail.com>
2024-12-08 20:51:27 -0500

    contra

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ const loginController = (req, res) => {
   const { password } = req.body;
   //   console.log("password: ", password);
   // Validar contraseña
-  if (password === "Alejo123") {
+  if (password === "Alejo123*-+") {
     // Aquí comparas la contraseña
     const token = jwt.sign({ user: "user" }, SECRET_KEY, { expiresIn: "1h" });
     res.status(200).json({ token });
